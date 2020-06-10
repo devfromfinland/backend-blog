119c30fad68ee56b298428e4b08098c648df21f9
devfromfinland <contact@vietphan.info>
2020-06-10 12:23:52 +0300

    style: refactor app.js and index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-// const http = require('http')
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
-const app = express()
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
