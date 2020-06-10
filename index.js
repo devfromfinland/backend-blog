9c5a8eeb7aa169b69d7093c516a2ddc60f158de2
devfromfinland <contact@vietphan.info>
2020-06-10 11:55:52 +0300

    style: refactor 'blog' model

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 // const http = require('http')
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const app = express()
 
 app.use(cors())
 app.use(express.json())
