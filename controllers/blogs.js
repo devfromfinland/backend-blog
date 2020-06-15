2c99515563256291b6f3bf9be2ba7bc5a4f7cde2
devfromfinland <contact@vietphan.info>
2020-06-15 23:26:57 +0300

    submit: update exercise 4.19 and complete 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,13 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1, name: 1 })
@@ -28,32 +21,29 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const { user, title, author, url, likes } = request.body
+  const { title, author, url, likes } = request.body
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
+  // console.log('token', request.token)
+  // console.log('decoded token, id: ', decodedToken.id)
 
-  console.log('passed authentication')
-
-  let updateUser = await User.findById(user)
+  let updateUser = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title,
     author,
     url,
     likes,
-    user,
+    user: decodedToken.id,
   })
-
-  console.log('blog to save', blog)
+  // console.log('blog to save', blog)
 
   const savedBlog = await blog.save()
-
-  console.log('saved blog', savedBlog)
+  // console.log('saved blog', savedBlog)
   updateUser.blogs = updateUser.blogs.concat(savedBlog._id)
   await updateUser.save()
 
